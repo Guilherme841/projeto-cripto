0940a87f435b1746e34f2804f252823aacfc8ceb
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-04-28 23:03:42 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -19,55 +19,62 @@ const grafico = document.getElementById("grafico").getContext("2d");
 })();
 
 function buscarBitcoin(real) {
+  var precoBrl = real;
+
+  let dias = 7;
   fetch(
     `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=CNY&apikey=4V0TJJQB3286WVE8`
   )
     .then((response) => response.json())
     .then((data) => {
       if (data.erro) {
-        alert("ERRO!");
+        alert("ERRO");
       } else {
-        let bitcoinLast = Object.keys(
+        let datas = Object.keys(
           data["Time Series (Digital Currency Daily)"]
-        )[0];
-        let ultimoPreco =
-          data["Time Series (Digital Currency Daily)"][bitcoinLast][
-            "4b. close (USD)"
-          ];
-        let preçoNumber = Number(ultimoPreco);
-        let precoBrl = (preçoNumber * real).toLocaleString("pt-BR", {
-          style: "currency",
-          currency: "BRL",
+        ).slice(0, dias);
+        let precos = datas.map(
+          (date) =>
+            data["Time Series (Digital Currency Daily)"][date][
+              "4b. close (USD)"
+            ]
+        );
+
+        let precosBitcoinBrl = precos.map((precoUsd) => {
+          precoUsd = (precoUsd * real)
+          return precoUsd;
         });
-        resposta.innerHTML = `O preço do bitcoin hoje é: ${precoBrl}`;
-        atualizarPreço(precoBrl)
+
+        let tempos = datas.map((date) => new Date(date).toLocaleDateString());
+
+        const chartData = {
+          labels: tempos,
+          datasets: [
+            {
+              label: "Preço do Bitcoin",
+              data: precosBitcoinBrl,
+              backgroundColor: "rgba(255, 99, 132, 0.2)",
+              borderColor: "rgba(255, 99, 132, 1)",
+              borderWidth: 1,
+            },
+          ],
+        };
+
+        // Obtenha o contexto do canvas do gráfico
+        const grafico = document.getElementById("grafico").getContext("2d");
+
+        // Crie um novo gráfico de linha com o Chart.js
+        const meuGrafico = new Chart(grafico, {
+          type: "line",
+          data: chartData,
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        });
       }
     });
 }
-
-function atualizarPreço(precoBrl) {
-  let precoBrlBtc = precoBrl;
-  const meuGrafico = new Chart(grafico, {
-    type: "line",
-    data: {
-      labels: ["teste"],
-      datasets: [
-        {
-          label: precoBrlBtc,
-          data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          borderColor: "rgba(255, 99, 132, 1)",
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
-        },
-      },
-    },
-  });
-}
-
