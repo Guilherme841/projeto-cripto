bd5a97fbf8b15c36ef6d73ffc724ffe47f92563f
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-05-08 22:50:05 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const grafico = document.getElementById("grafico").getContext("2d");
 const preçoBtc = document.getElementById("preçoBtc");
 const moeda = document.getElementById("moeda");
 
+let dadosFetch;
+let tipoDeMoeda = moeda.value;
+let dias = 5;
+
 (function () {
   fetch(
     `https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados/ultimos/1?formato=json`
@@ -13,19 +17,25 @@ const moeda = document.getElementById("moeda");
       } else {
         let real = data[0].valor;
         buscarBitcoin(real);
+        trocarMoeda(real);
+      }
+    });
+  fetch(
+    `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=CNY&apikey=4V0TJJQB3286WVE8`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.erro) {
+        alert("ERRO");
+      } else {
+        dadosFetch = data;
       }
     });
 })();
 
-let tipoDeMoeda = moeda.value;
-
-function trocarMoeda() {
+function trocarMoeda(real) {
+  precoBrl = real;
   tipoDeMoeda = moeda.value;
-}
-
-function buscarBitcoin(real) {
-  var precoBrl = real;
-  let dias = 5;
   fetch(
     `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=CNY&apikey=4V0TJJQB3286WVE8`
   )
@@ -56,18 +66,59 @@ function buscarBitcoin(real) {
         });
 
         let preçoDoDiaUsd = precos[0];
-        let preçoDoDiaUsdCon = preçoDoDiaUsd.toLocaleString.toLocaleString(
-          "en-US",
-          { style: "currency", currency: "USD" }
+        let preçoDoDiaUsdCon = preçoDoDiaUsd.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        });
+
+        let moedaBtc;
+
+        if (tipoDeMoeda.value === "BRL") {
+          moedaBtc = preçoDoDiaBrlCon;
+        } else {
+          moedaBtc = preçoDoDiaUsdCon;
+        }
+        moeda.innerHTML = `${moedaBtc}`;
+      }
+    });
+}
+
+function buscarBitcoin(real) {
+  precoBrl = real;
+  fetch(
+    `https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=CNY&apikey=4V0TJJQB3286WVE8`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.erro) {
+        alert("ERRO");
+      } else {
+        let datas = Object.keys(
+          data["Time Series (Digital Currency Daily)"]
+        ).slice(0, dias);
+        let precos = datas.map(
+          (date) =>
+            data["Time Series (Digital Currency Daily)"][date][
+              "4b. close (USD)"
+            ]
         );
 
-        preçoBtc.innerHTML = `${preçoDoDiaBrlCon}`;
+        let precosBitcoinBrl = precos.map((precoUsd) => {
+          precoUsd = precoUsd * precoBrl;
+          return precoUsd;
+        });
+
+        let preçoDoDiaBrl = precosBitcoinBrl[0];
+        let preçoDoDiaBrlCon = preçoDoDiaBrl.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        });
 
         const chartData = {
           labels: datas,
           datasets: [
             {
-              label: "Preço do Bitcoin em brl",
+              label: "Preço do Bitcoin em BRL",
               data: precosBitcoinBrl,
               backgroundColor: "rgba(255, 99, 132, 0.2)",
               borderColor: "rgba(255, 99, 132, 1)",
